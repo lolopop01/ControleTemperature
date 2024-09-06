79a9f11d59553dcfd98e14a2b90c64edbf3f706d
lolopop1 <zacharie-cote@hotmail.ca>
2024-09-06 11:48:53 -0400

    Did code

---
+++ Main.py
@@ -0,0 +1,59 @@
+import time
+import paho.mqtt.client as mqtt
+
+# Configuration de MQTT
+broker = "10.4.1.202"
+port = 1883
+temperature_topic = "paho/Zach/Temp"
+humidity_topic = "paho/Zach/Humidity"
+
+
+def on_connect(client, userdata, flags, rc):
+    """
+    Callback function called when the client connects to the broker.
+
+    :param client: The client instance for this callback.
+    :param userdata: The private user data as set in Client() or passed to user_data_set().
+    :param flags: Response flags sent by the broker.
+    :param rc: The connection result code.
+    """
+    print("Connecté avec le code : " + str(rc))
+    # Abonnement aux topics
+    client.subscribe(temperature_topic, qos=0)
+    client.subscribe(humidity_topic, qos=0)
+
+
+def on_message(client, userdata, msg):
+    """
+    Callback function called when a PUBLISH message is received from the broker.
+
+    :param client: The client instance for this callback.
+    :param userdata: The private user data as set in Client() or passed to user_data_set().
+    :param msg: An instance of MQTTMessage. This is a class with members:
+                - payload: The message payload (byte array)
+                - topic: The topic of the message
+                - qos: The QoS level of the message
+                - retain: Boolean flag indicating if the message is retained
+    """
+    # Traitement des messages reçus
+    if msg.topic == temperature_topic:
+        print(f"Température reçue : {msg.payload.decode()}")
+    elif msg.topic == humidity_topic:
+        print(f"Humidité reçue : {msg.payload.decode()}")
+
+
+client = mqtt.Client()  # Create a new instance of the MQTT client
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker, port, 60)
+client.loop_start()
+
+try:
+    while True:
+        time.sleep(1)  # Attendre en attendant les messages
+except KeyboardInterrupt:
+    print("Interruption par l'utilisateur")
+finally:
+    client.loop_stop()
+    client.disconnect()
